c4107183ec95e15e79a64b6cb1330b9beacf5dc7
andreakiro <andreakiro.pinto@gmail.com>
2022-11-22 09:11:12 +0100

    Minor format updates for PR#1

--- very_simple_regression/utils.py
+++ very_simple_regression/utils.py
@@ -1,12 +1,11 @@
-import numpy as np
-import time
-import os
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def create_noisy_sinus(num_samples):
-    
+
     folder_path = create_data_folder()
     data_path = os.path.join(folder_path, "noisy_sinus.npy")
     
@@ -17,22 +16,18 @@ def create_noisy_sinus(num_samples):
     data = np.array([inputs, observations, ground_truth])
     np.save(data_path, data)
     
-    
 def create_data_folder():
     folder_path = os.path.join(ROOT_DIR, "data")
     os.makedirs(folder_path, exist_ok=True)
     return folder_path
 
-
 def load_noisy_sinus():
     data = np.load(os.path.join(os.path.join(ROOT_DIR, "data"), "noisy_sinus.npy"))
     return data
 
-
 def plot(x, observations, ground_truth=None):
     plt.scatter(x, observations, color="r", label="noisy observation", marker='.')
     if ground_truth is not None: 
-        print("plot")
         plt.plot(ground_truth[0], ground_truth[1], color="blue", label="ground truth")
     plt.xlabel('x')
     plt.ylabel('sin(x)')
