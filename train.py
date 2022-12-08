0fcc2bec287a46b3788e86b3c457bb1b1dfc5f99
andreakiro <andreakiro.pinto@gmail.com>
2022-12-08 15:28:45 +0100

    Structural and cosmetic changes to handle clf, trf models

--- train.py
+++ train.py
@@ -1,175 +1,165 @@
+import os, re, json, argparse, torch, random
 from torch.utils.data import random_split, DataLoader
+from easydict import EasyDict as edict
 from datetime import datetime
-import argparse
-import torch
-import json
-import os
-import re
-import numpy as np
-import random
 import numpy as np
 
-from src.utils import create_noisy_sinus, plot, create_model_save_folder
+from src.utils import create_noisy_sinus, plot
 from src.mlp.datasets import SinusDataset
 from src.mlp.trainers import BPTrainer, PCTrainer
 from src.mlp.models.regression import BPSimpleRegressor, PCSimpleRegressor
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-OUT_DIR = os.path.join(ROOT_DIR, 'out')
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+OUT_DIR  = os.path.join(ROOT_DIR, 'out')
+
+torch.manual_seed(0)
+random.seed(0)
+np.random.seed(0)
+
 
 def read_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--run', help=f"Individual run name, if reused the training is resumed", required=True, type=str)
+    
+    # minimal required arguments:
     parser.add_argument('-m', '--model', help=f"Model selection for experiments", choices={'reg', 'clf', 'trf'}, required=True, type=str)
     parser.add_argument('-t','--training', help=f"Training framework, either 'bp' (backprop) or 'pc' (predictive coding)", choices={'bp', 'pc'}, required=True, type=str)
-    parser.add_argument('-n','--num', help=f"Number of generated samples", required=False, default=1000, type=int)
-    parser.add_argument('-l','--lr', help=f"Learning rate", required=False, default=0.001, type=float)
-    parser.add_argument('-c','--clr', help=f"PC convergence learning rate", required=False, default=0.2, type=float)
+    
+    # common to all architectures and training modes:
+    parser.add_argument('-bs','--batch-size', help=f"Batch size used for training and evaluation", required=False, default=32, type=int)
     parser.add_argument('-e','--epochs', help=f"Training epochs", required=False, default=300, type=int)
-    parser.add_argument('-it','--iterations', help=f"PC convergence iterations", required=False, default=100, type=int)
-    parser.add_argument('-p','--plot', help=f"Plot the results after training or not", required=False, default=False, type=bool)
-    parser.add_argument('-v','--verbose', help=f"Verbosity level", required=False, default=0, type=int)
-    parser.add_argument('-i','--init', help=f"PC initialization technique", choices={'zeros', 'normal', 'xavier_normal', 'forward'}, required=False, default="forward", type=str)
     parser.add_argument('-dp','--dropout', help=f"Dropout level", required=False, default=0, type=float)
-    parser.add_argument('-cf','--checkpoint_frequency', help=f"checkpoint frequency in epochs", required=False, default=1, type=int)
+    parser.add_argument('-l','--lr', help=f"Learning rate", required=False, default=0.001, type=float)
+    parser.add_argument('-v','--verbose', help=f"Verbosity level", required=False, default=0, type=int)
     parser.add_argument('-es','--early_stopping', help=f"the number of past epochs taken into account for early_stopping", required=False, default=300, type=int)
-    parser.add_argument('-b','--batch-size', help=f"Batch size used for training and evaluation", required=False, default=32, type=int)
+
+    # PC training mode specific:
+    parser.add_argument('-i','--init', help=f"PC initialization technique", choices={'zeros', 'normal', 'xavier_normal', 'forward'}, required=False, default="forward", type=str)
+    parser.add_argument('-c','--clr', help=f"PC convergence learning rate", required=False, default=0.2, type=float)
+    parser.add_argument('-it','--iterations', help=f"PC convergence iterations", required=False, default=100, type=int)
+
+    # io, logging and others:
     parser.add_argument('-lg','--log', help=f"Log info and results of the model or not", required=False, default=False, type=bool)
-    args = vars(parser.parse_args())
-    return args
+    parser.add_argument('-cf','--checkpoint_frequency', help=f"checkpoint frequency in epochs", required=False, default=1, type=int)
+    parser.add_argument('-p','--plot', help=f"Plot the results after training or not", required=False, default=False, type=bool)
+    parser.add_argument('-ns','--nsamples', help=f"Number of generated samples for regression", required=False, default=1000, type=int)
+    # parser.add_argument('-r', '--run', help=f"Individual run name, if reused the training is resumed", required=True, type=str)
+
+    return edict(vars(parser.parse_args()))
 
 
 def main():
-    torch.manual_seed(0)
-    random.seed(0)
-    np.random.seed(0)
 
+    # fetch run args
     args = read_arguments()
-    lr = args['lr']
-    clr = args['clr']
-    epochs = args['epochs']
-    iterations = args['iterations']
-    verbose = args['verbose']
-    train = args['training']
-    arg_plot = args['plot']
-    dropout = args['dropout']
-    init = args['init']
-    run_name = args['run']
-    early_stopping = args["early_stopping"]
-    checkpoint_frequency = args['checkpoint_frequency']
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model_type = args['model']
-    batch_size = args['batch_size']
-    log = args['log']
+    dt_string = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # saving paths
-    model_save_dir = create_model_save_folder(args["model"], run_name)
-    log_dir = os.path.join(OUT_DIR, 'logs', args['model'], run_name)
-    os.makedirs(log_dir, exist_ok=True)
-    image_dir = os.path.join(OUT_DIR, 'images', args['model'], run_name)
-    os.makedirs(image_dir, exist_ok=True)
-    data_path = os.path.join(ROOT_DIR, "src/data/noisy_sinus.npy")
+    # path to saving directories
+    logs_dir   = os.path.join(OUT_DIR, 'logs', args.model, dt_string)
+    plots_dir  = os.path.join(OUT_DIR, 'plots', args.model, dt_string)
+    models_dir = os.path.join(OUT_DIR, 'models', args.model, dt_string)
     
-    # prepare data and dataloaders
-    create_noisy_sinus(num_samples=args['num']) # create the data, if they don't exist
-    dataset = SinusDataset(data_path=data_path, device=device)
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    training_data, val_data = random_split(dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42))
+    # safely create directories
+    os.makedirs(logs_dir, exist_ok=True)
+    os.makedirs(models_dir, exist_ok=True)
 
-    train_dataloader = DataLoader(training_data, batch_size=batch_size)
-    val_dataloader = DataLoader(val_data, batch_size=batch_size)
 
-    if model_type == 'reg':
-        if train == 'bp':
-            model = BPSimpleRegressor(dropout=dropout).to(device)
-        elif train == 'pc':
-            model = PCSimpleRegressor(dropout=dropout).to(device)
-    elif model_type == 'clf':
+    if args.model == 'reg':
+        create_noisy_sinus(outdir=DATA_DIR, num_samples=args.nsamples)
+        dpath = os.path.join(DATA_DIR, 'regression', 'noisy_sinus.npy')
+        sinus_dataset = SinusDataset(data_path=dpath, device=device)
+        train_size = int(0.8 * len(sinus_dataset))
+        val_size = len(sinus_dataset) - train_size
+        train_data, val_data = random_split(sinus_dataset, [train_size, val_size], generator=torch.Generator())
+        train_loader = DataLoader(train_data, batch_size=args.batch_size)
+        val_loader = DataLoader(val_data, batch_size=args.batch_size)
+        
+        if args.training == 'bp': model = BPSimpleRegressor(dropout=args.dropout)
+        elif args.training == 'bp': model = PCSimpleRegressor(dropout=args.dropout)
+
+    elif args.model == 'clf':
         raise NotImplementedError("Classifier models are not implemented yet")
-    elif model_type == 'trf':
+
+    elif args.model == 'trf':
         raise NotImplementedError("Transformer models are not implemented yet")
+
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    loss = torch.nn.MSELoss()
 
     # TODO Luca mentioned adam is not suitable for PC
     # we might have to change this to SGD if it performs bad on PC
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    loss = torch.nn.MSELoss()
-        
-    # load last model checkpoint, optimizer checkpoint and past validation losses if existent
-    start_epoch = 0
-    checkpoints = [checkpoint for checkpoint in os.listdir(model_save_dir) if checkpoint.startswith("checkpoint_")]
-    validation_losses = []
-    train_losses = []
-    if len(checkpoints) != 0:
-        last_epoch = max([int(re.search(r'\d+', file).group()) for file in checkpoints])
-        start_epoch = last_epoch + 1
-        last_checkpoint = torch.load(f = os.path.join(model_save_dir, f"checkpoint_{last_epoch}.pt"))
-        model.load_state_dict(last_checkpoint['model_state_dict'])
-        optimizer.load_state_dict(last_checkpoint['optimizer_state_dict'])
-        validation_losses = np.load(os.path.join(log_dir, f"validation_losses.npy")).tolist()
-        train_losses = np.load(os.path.join(log_dir, f"train_losses.npy")).tolist()
-    model.to(device)
+    # TODO: add reload from checkpoint (not necessary)
     
-    # init Trainers
-    if train == "bp":
+    if args.training == 'bp':
         trainer = BPTrainer(
-            optimizer=optimizer, 
-            loss=loss,
-            checkpoint_frequency=checkpoint_frequency, 
-            device=device, 
-            epochs=epochs, 
-            early_stopping=early_stopping,
-            val_loss=validation_losses,
-            train_loss=train_losses,
-            model_save_folder=model_save_dir,
-            log_save_folder=log_dir, 
-            verbose=verbose)
-    elif train == 'pc':
+            # essentials
+            device    = device,
+            epochs    = args.epochs,
+            optimizer = optimizer,
+            loss      = loss,
+            early_stp = args.early_stopping,
+            # io related
+            verbose   = args.verbose,
+            cp_freq   = args.checkpoint_frequency,
+            model_dir = models_dir,
+            log_dir   = logs_dir, 
+        )
+
+    elif args.training == 'pc':
         trainer = PCTrainer(
-            optimizer=optimizer, 
-            loss=loss, 
-            device=device, 
-            init=init, 
-            epochs=epochs, 
-            iterations=iterations,
-            clr=clr,
-            verbose=verbose)
+            # essentials
+            device     = device,
+            epochs     = args.epochs,
+            optimizer  = optimizer, 
+            loss       = loss,
+            init       = args.init,
+            iterations = args.iterations,
+            clr        = args.clr,
+            # io related
+            verbose    = args.verbose
+        )
+
     
-    print(f"[Training started]")
-    stats = trainer.fit(model, train_dataloader, val_dataloader, start_epoch)
-    print(f"\n[Training completed]")
-    print(f'{"Number of epochs": <21}: {epochs}')
+    print(f"[Training is starting]")
+    stats = trainer.fit(model, train_loader, val_loader, 0)
+
+    print(f"\n[Training is complete]")
+    print(f'{"Number of epochs": <21}: {args.epochs}')
     print(f'{"Elapsed time": <21}: {round(stats["time"], 2)}s')
     print(f'{"Best train loss": <21}: {round(stats["best_train_loss"], 5)}')
     print(f'{"Best validation loss": <21}: {round(stats["best_val_loss"], 5)}')
     print(f'{"Best epoch": <21}: {stats["best_epoch"]}')
 
-    # evaluate
-    X, y = [], []
-    for batch, _ in val_dataloader:
-        X.append(batch.detach().numpy())
-        y.append(model(batch).detach().numpy())
-    X, y = np.concatenate(X).ravel(), np.concatenate(y).ravel()
-    dt_string = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # visualize predictions on validation
-    if arg_plot:
-        outfile = os.path.join(image_dir, dt_string+'.png')
-        plot(X, y, dataset.gt, outfile=outfile if log else None)
-    
+    if args.plot and args.model == 'reg':
+        os.makedirs(plots_dir, exist_ok=True)
+
+        X, y = [], []
+        for batch, _ in val_loader:
+            X.append(batch.detach().numpy())
+            y.append(model(batch).detach().numpy())
+        X, y = np.concatenate(X).ravel(), np.concatenate(y).ravel()
+        # TODO 5 previous lines should be a defined function
+
+        outfile = os.path.join(os.path.dirname(plots_dir), dt_string + '.png')
+        plot(X, y, sinus_dataset.gt, outfile=outfile if log else None)
+
+
     # save model run parameters
-    if log:
-        outfile = os.path.join(log_dir, dt_string+'.json')
-        log = {
-            "framework" : train,
-            "epochs" : epochs,
-            "optimizer" : type (optimizer).__name__,
-            "loss" : loss._get_name(),
-            "lr" : lr,
-            "results" : stats
-        }
-        with open(outfile, 'w') as f:
-            json.dump(log, f, indent=2)
+    logfile = os.path.join(logs_dir, 'runlogs.json')
+    log = {
+        "framework" : args.training,
+        "epochs" : args.epochs,
+        "optimizer" : type (optimizer).__name__,
+        "loss" : loss._get_name(),
+        "lr" : args.lr,
+        "results" : stats
+    }
+    with open(logfile, 'w') as f:
+        json.dump(log, f, indent=2)
+
 
 if __name__ == "__main__":
     main()
