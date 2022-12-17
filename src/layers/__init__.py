bffd89d1fb21c80d724c6f2ced0a24a12d54bd89
andreakiro <andreakiro.pinto@gmail.com>
2022-12-17 13:58:25 +0100

    Remove transformers layers from module

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -1,9 +1,9 @@
 from .pc_layer import PCLayer
-from .pc_multi_head_attention import PCMultiheadAttention
-from .pc_encoder_layer import PCTransformerEncoderLayer
+# from .pc_multi_head_attention import PCMultiheadAttention
+# from .pc_encoder_layer import PCTransformerEncoderLayer
 
 __all__ = [
     'PCLayer', 
-    'PCMultiheadAttention', 
-    'PCTransformerEncoderLayer'
+    # 'PCMultiheadAttention', 
+    # 'PCTransformerEncoderLayer'
 ]
