Fixing issue w/ PC initialization, softmax and output fix, now working fine

src/layers/pc_layer.py
@@ -104,7 +104,7 @@ class PCSoftmaxLayer(PCLayer):
 
     """
     def __init__(self, size: int) -> None:
-        super(PCLayer, self).__init__(size)
+        super().__init__(size)
 
     def forward(self, μ: torch.Tensor, init) -> torch.nn.Parameter:
         """
@@ -137,7 +137,7 @@ class PCSoftmaxLayer(PCLayer):
 
         """
         if init is not None: self.init(init=init, μ=μ)
-        x = torch.softmax(x, dim=1)
+        x = torch.nn.Parameter(torch.softmax(self.x, dim=1))
         μ = torch.softmax(μ, dim=1)
 
         self.ε = torch.sum(torch.square(x - μ), dim=1)

src/mlp/models/classification.py
@@ -52,7 +52,7 @@ class PCSimpleClassifier(nn.Module):
         self.pc_softmax = PCSoftmaxLayer(size=10)
 
         self.linear_layers = [self.linear_1, self.linear_2, self.linear_3]
-        self.pc_layers = [self.pc_layer1, self.pc_layer2, self.pc_layer3]
+        self.pc_layers = [self.pc_layer1, self.pc_layer2, self.pc_layer3, self.pc_softmax]
 
 
     def forward(self, input, init=None) -> torch.Tensor:
@@ -109,5 +109,6 @@ class PCSimpleClassifier(nn.Module):
         
         """
         output = F.one_hot(output, num_classes=10).type(torch.FloatTensor)
+        output *= 1e+30
         self.pc_layers[-1].x = torch.nn.Parameter(output)
         